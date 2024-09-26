750bb626fa394467aa3dfe4b6c092fc3c21f84e5
icedaman <36274654+icedaman@users.noreply.github.com>
2024-09-26 21:35:54 +0100

    Navbar links router setup

--- job-listings/src/components/JobListings.vue
+++ job-listings/src/components/JobListings.vue
@@ -2,6 +2,7 @@
   import jobData from '@/jobs.json';
   import { ref, defineProps } from 'vue';
   import JobListing from './JobListing.vue';
+  import { RouterLink } from 'vue-router';
 
   const jobs = ref(jobData);
 
@@ -16,7 +17,7 @@
 </script>
 
 <template>
-  <section class="bg-blue-100 px-4 py-10">
+  <section class="bg-blue-50 px-4 py-10">
     <div class="container-xl lg:container m-auto">
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
@@ -27,10 +28,10 @@
     </div>
   </section>
   <section v-if="showMoreBtn" class="m-auto max-w-lg my-10 px-6">
-    <a
-      href="/jobs"
+    <RouterLink
+      to="/jobs"
       class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-      >View All Jobs</a
-    >
+      >View All Jobs
+    </RouterLink>
   </section>
 </template>
